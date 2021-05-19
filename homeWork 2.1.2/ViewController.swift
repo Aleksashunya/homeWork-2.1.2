893542ab941d8b1bdd5de904d11af30f7b9a3432
Sasha <pusmi.1987@gmail.com>
2021-05-19 21:46:49 +1000

    Added objects and their functionality

--- homeWork 2.1.2/ViewController.swift
+++ homeWork 2.1.2/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var redColorView: UIView!
+    @IBOutlet weak var yellowColorView: UIView!
+    @IBOutlet weak var greenColorView: UIView!
+    
+    @IBOutlet weak var startButtom: UIButton!
+    
+    var lightOn: CGFloat = 1
+    var lightOff: CGFloat = 0.3
+    var step = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColorView.layer.cornerRadius = 50
+        yellowColorView.layer.cornerRadius = 50
+        greenColorView.layer.cornerRadius = 50
+        
+        startButtom.layer.cornerRadius = 10
+        
+        redColorView.alpha = lightOff
+        yellowColorView.alpha = lightOff
+        greenColorView.alpha = lightOff
     }
 
-
+    @IBAction func tappedStartButtom(_ sender: Any) {
+        startButtom.setTitle("NEXT", for: .normal)
+        step += 1
+        
+        switch step {
+        case 1:
+            greenColorView.alpha = lightOff
+            redColorView.alpha = lightOn
+        case 2:
+            redColorView.alpha = lightOff
+            yellowColorView.alpha = lightOn
+        case 3:
+            yellowColorView.alpha = lightOff
+            greenColorView.alpha = lightOn
+            step = 0
+        default: return
+        }
+    }
 }
+
 
